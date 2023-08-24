7f0581f2f854bcd86b40b2bd06dfbb8ad81dc954
the1nekev <kevcorcino15@gmail.com>
2023-08-24 13:01:17 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -60,6 +60,7 @@ const getToken = async (code) => {
 };
 
 export const getEvents = async () => {
+  NProgress.start();
   if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return mockData;
